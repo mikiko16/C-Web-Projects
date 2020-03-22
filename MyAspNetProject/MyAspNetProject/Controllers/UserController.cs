4fa7a5d167278a6f94be558902140416590c2c55
Miroslav Dranchovski <mikiko16@abv.bg>
2020-03-22 07:49:44 +0200

    Implements JWT !
    Issue and Audience not working properly !!!

--- MyAspNetProject/MyAspNetProject/Controllers/UserController.cs
+++ MyAspNetProject/MyAspNetProject/Controllers/UserController.cs
@@ -1,14 +1,19 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Cors;
+﻿using Microsoft.AspNetCore.Cors;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using MyAspNetProject.Data;
+using MyAspNetProject.Helpers;
 using MyAspNetProject.models;
+using MyAspNetProject.JWT;
 using MyAspNetProject.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Security.Claims;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 
 namespace MyAspNetProject.Controllers
 {
@@ -21,16 +26,26 @@ namespace MyAspNetProject.Controllers
         private readonly UserManager<UserApp> userManager;
         private readonly SignInManager<UserApp> signInManager;
         private readonly RoleManager<IdentityRole> roleManager;
+        private readonly IJwtFactory _jwtFactory;
+        private readonly JwtIssuerOptions _jwtOptions;
+        private readonly ClaimsPrincipal _caller;
+        UserApp userToVerify;
 
-        public UserController(ApplicationDbContext db, 
-                              UserManager<UserApp> userManager, 
+        public UserController(ApplicationDbContext db,
+                              UserManager<UserApp> userManager,
                               SignInManager<UserApp> signInManager,
-                              RoleManager<IdentityRole> roleManager)
+                              RoleManager<IdentityRole> roleManager,
+                              IJwtFactory jwtFactory,
+                              IOptions<JwtIssuerOptions> jwtOptions,
+                              IHttpContextAccessor httpContextAccessor)
         {
             this.db = db;
             this.userManager = userManager;
             this.signInManager = signInManager;
             this.roleManager = roleManager;
+            this._jwtFactory = jwtFactory;
+            this._jwtOptions = jwtOptions.Value;
+            this._caller = httpContextAccessor.HttpContext.User;
         }
 
         [HttpPut]
@@ -47,6 +62,7 @@ namespace MyAspNetProject.Controllers
 
 
         [HttpGet]
+        [Authorize]
         [Route("All")]
         public ActionResult<IEnumerable<IdentityUser>> GetNotActiveUsers()
         {
@@ -60,15 +76,16 @@ namespace MyAspNetProject.Controllers
             var result = await this.signInManager.PasswordSignInAsync(model.Email,
                 model.PasswordHash, true, false);
 
-            var user = this.User;
+            var identity = await GetClaimsIdentity(model.Email, model.PasswordHash);
 
-            if (this.User.IsInRole("Admin"))
-            {
-                return BadRequest();
-            }
             if (result.Succeeded)
             {
-                return Ok(result);
+                //var user = await userManager.FindByNameAsync(model.Email);
+
+                var isAdmin = await userManager.IsInRoleAsync(this.userToVerify, "Admin");
+
+                var jwt = await Tokens.GenerateJwt(identity, _jwtFactory, model.Email, _jwtOptions, new JsonSerializerSettings { Formatting = Formatting.Indented }, isAdmin);
+                return Ok(jwt);
             }
 
             return Unauthorized(result);
@@ -93,26 +110,47 @@ namespace MyAspNetProject.Controllers
 
             var result = await this.userManager.CreateAsync(newUser, model.PasswordHash);
 
-            if (result.Succeeded) 
+            if (result.Succeeded)
             {
                 if (!hasCompany)
                 {
                     var res = await userManager.AddToRoleAsync(newUser, "Admin");
                     return Ok(res);
                 }
-                return Ok(result);
+                return new OkObjectResult("Account Created");
             }
             return BadRequest(result);
         }
 
         [HttpPost]
+        [Authorize ( Policy = "ApiUser")]
         [Route("Logout")]
 
-        public async Task<ActionResult> Logout()
+        public async Task<IActionResult> Logout()
         {
-            await this.signInManager.SignOutAsync();
+           // await this.signInManager.SignOutAsync();
 
-            return Ok();
+            return Ok("Miro is the best !!!");
+        }
+
+        private async Task<ClaimsIdentity> GetClaimsIdentity(string userName, string password)
+        {
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(password))
+                return await Task.FromResult<ClaimsIdentity>(null);
+
+            // get the user to verifty
+            this.userToVerify = await userManager.FindByNameAsync(userName);
+
+            if (userToVerify == null) return await Task.FromResult<ClaimsIdentity>(null);
+
+            // check the credentials
+            if (await userManager.CheckPasswordAsync(userToVerify, password))
+            {
+                return await Task.FromResult(_jwtFactory.GenerateClaimsIdentity(userName, userToVerify.Id));
+            }
+
+            // Credentials are invalid, or account doesn't exist
+            return await Task.FromResult<ClaimsIdentity>(null);
         }
     }
 }
--- MyAspNetProject/MyAspNetProject/Startup.cs
+++ MyAspNetProject/MyAspNetProject/Startup.cs
@@ -1,20 +1,27 @@
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
 using MyAspNetProject.Data;
+using MyAspNetProject.Helpers;
+using MyAspNetProject.JWT;
 using MyAspNetProject.models;
-using System.Web.Http.Cors;
+using System;
+using System.Text;
 
 namespace MyAspNetProject
 {
     public class Startup
     {
+        private const string SecretKey = "iNivDmHLpUA223sqsfhqGbMRdRj1PVkH";
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SecretKey));
+
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
@@ -35,11 +42,72 @@ namespace MyAspNetProject
                 facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
             });
 
+            services.AddSingleton<IJwtFactory, JwtFactory>();
+
+            var jwtAppSettingOptions = Configuration.GetSection(nameof(JwtIssuerOptions));
+
+            services.Configure<JwtIssuerOptions>(options =>
+            {
+                options.Issuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                options.Audience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)];
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)],
+
+                ValidateAudience = false,
+                ValidAudience = jwtAppSettingOptions[nameof(JwtIssuerOptions.Audience)],
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = false,
+                ValidateLifetime = true,
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(configureOptions =>
+            {
+                configureOptions.ClaimsIssuer = jwtAppSettingOptions[nameof(JwtIssuerOptions.Issuer)];
+                configureOptions.TokenValidationParameters = tokenValidationParameters;
+                configureOptions.SaveToken = true;
+            });
+
+            //services.AddDataAnnotations();
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("ApiUser", policy => policy.RequireClaim(Constants.Strings.JwtClaimIdentifiers.Rol, Constants.Strings.JwtClaims.ApiAccess));
+            });
+
             services.AddHealthChecks();
 
             services.AddCors(options => {
-                options.AddPolicy("AllowAll", builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+                options.AddPolicy("AllowAll", builder => builder
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
             });
+
+            var builder = services.AddIdentityCore<UserApp>(o =>
+            {
+                // configure identity options
+                o.Password.RequireDigit = false;
+                o.Password.RequireLowercase = false;
+                o.Password.RequireUppercase = false;
+                o.Password.RequireNonAlphanumeric = false;
+                o.Password.RequiredLength = 6;
+            });
+
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), builder.Services);
+            builder.AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
 
             services.AddDbContext<ApplicationDbContext>(options => 
             options.UseSqlServer(DatabaseConfiguration.ConnectionString));
@@ -51,6 +119,8 @@ namespace MyAspNetProject
             })
                 .AddRoles<IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+
+            //services.AddMvc().AddFluentValidation(fv => fv.RegisterValidatorsFromAssemblyContaining<Startup>());
         }
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
